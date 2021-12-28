82a3f383ee219680d37553fc4998d239f9694dfc
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2021-12-28 13:24:11 +0530

    Add files via upload

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,37 @@
+package pra;
+import java.util.*;
+public class MaximumSubarraySum {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		System.out.println("enter the number of elements in the array:");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		
+		//logic
+		int sum=0;
+		int ans=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				sum=0;
+				for(int k=i;k<=j;k++)
+				{
+					//System.out.print(arr[k]+" ");
+					sum=sum+arr[k];
+				}
+				//System.out.println();
+				if(sum>ans)ans=sum;
+				
+			}
+		}
+		System.out.println("Maximum SubArray Sum: "+ans );
+	}
+}
