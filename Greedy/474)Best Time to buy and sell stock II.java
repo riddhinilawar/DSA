2b22623af877ad629b2d1db4c65f47f1cf9c7100
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-06-12 23:02:38 +0530

    Update 474)Best Time to buy and sell stock II.java

--- Greedy/474)Best Time to buy and sell stock II.java
+++ Greedy/474)Best Time to buy and sell stock II.java
@@ -31,3 +31,16 @@ class Solution {
         return ans;
     }
 }
+
+
+--------------------------------------------------------------------------------------------------------------------------------------
+    
+ public class Solution {
+    public int maxProfit(int[] prices) {
+        int p = 0;
+        for (int i = 1; i < prices.length; i ++) {
+            p += Math.max(0, prices[i] - prices[i-1]);
+        }
+        return p;
+    }
+}
