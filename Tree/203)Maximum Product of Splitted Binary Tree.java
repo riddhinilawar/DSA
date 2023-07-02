3b5095b632df42b9322a495abe06227c0e81cc01
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-02 22:26:25 +0530

    Create 203)Maximum Product of Splitted Binary Tree.java

---
+++ Tree/203)Maximum Product of Splitted Binary Tree.java
@@ -0,0 +1,48 @@
+1339. Maximum Product of Splitted Binary Tree
+Medium
+2.7K
+99
+Companies
+Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of the sums of the subtrees is maximized.
+Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+Note that you need to maximize the answer before taking the mod and not after taking it.
+ 
+Example 1:
+ 
+Input: root = [1,2,3,4,5,6]
+Output: 110
+Explanation: Remove the red edge and get 2 binary trees with sum 11 and 10. Their product is 110 (11*10)
+Example 2:
+ 
+Input: root = [1,null,2,3,4,null,null,5,6]
+Output: 90
+Explanation: Remove the red edge and get 2 binary trees with sum 15 and 6.Their product is 90 (15*6)
+ 
+Constraints:
+The number of nodes in the tree is in the range [2, 5 * 104].
+1 <= Node.val <= 104
+
+class Solution {
+    static long sum;
+    static long maxProd;
+    public static void dfs(TreeNode root) {
+        if(root == null) return;
+        sum += (long)root.val;
+        dfs(root.left);
+        dfs(root.right);
+    }
+    public static long checkMax(TreeNode root) {
+        if(root == null) return 0;
+        long l = checkMax(root.left);
+        long r = checkMax(root.right);
+        maxProd = Math.max(maxProd, (l + r + root.val) * (sum - l - r - root.val));
+        return l + r + root.val;
+    }
+    public int maxProduct(TreeNode root) {
+        sum = 0;
+        maxProd = 0;
+        dfs(root);
+        long rootProd = checkMax(root);
+        return (int)(maxProd % ((int)Math.pow(10, 9) + 7));
+    }
+}
