fd1bf200578fa618ff90511fba592292a5573840
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-02 13:10:11 +0530

    Update 177)Maximum Sum Path in Binary Tree.java

--- Tree/177)Maximum Sum Path in Binary Tree.java
+++ Tree/177)Maximum Sum Path in Binary Tree.java
@@ -1,51 +1,17 @@
-Maximum Sum Path in Binary Tree
+Maximum Path Sum in Binary Tree
 
-Problem Statement: Write a program to find the maximum sum path in a binary tree. A path in a binary tree is a sequence of nodes where every adjacent pair of nodes are connected by an edge. A node can only appear in the sequence at most once. A path need not pass from the root. We need to find the path with the maximum sum in the binary tree.
-Example:
-Input:
- 
-Output: The Max Path Sum for the Tree is 42
+Problem Statement: Write a program to find the maximum sum path in a binary tree from root to leaf.
 
 
-import java.util.*;
 
-class Node {
-    int val;
-    Node left, right;
-    Node(int val) {
-        this.val = val;
-        left = null;
-        right = null;
+    public void getMaxPathSum(Node root, int sum){
+        if(root==null){ 
+          ans=Math.max(ans,sum);
+          return;
+        }
+        getPath(root.left,sum+root.val);
+        getPath(root.right,sum+root.val);
     }
-}
-class TUF {
+    
 
-    public static int maxPathSum(Node root) {
-        int maxValue[] = new int[1];
-        maxValue[0] = Integer.MIN_VALUE;
-        maxPathDown(root, maxValue);
-        return maxValue[0];
-    }
-
-    public static int maxPathDown(Node node, int maxValue[]) {
-        if (node == null) return 0;
-        int left = Math.max(0, maxPathDown(node.left, maxValue));
-        int right = Math.max(0, maxPathDown(node.right, maxValue));
-        maxValue[0] = Math.max(maxValue[0], left + right + node.val);
-        return Math.max(left, right) + node.val;
-    }
-
-    public static void main(String args[]) {
-
-        Node root = new Node(-10);
-        root.left = new Node(9);
-        root.right = new Node(20);
-        root.right.left = new Node(15);
-        root.right.right = new Node(7);
-
-        int answer = maxPathSum(root);
-        System.out.println("The Max Path Sum for this tree is " + answer);
-
-    }
-}
 
