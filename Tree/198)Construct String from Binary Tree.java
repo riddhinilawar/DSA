cba876ca7af1631a209c9933dd753c80e56ca78e
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-02 21:13:09 +0530

    Create 198)Construct String from Binary Tree.java

---
+++ Tree/198)Construct String from Binary Tree.java
@@ -0,0 +1,48 @@
+606. Construct String from Binary Tree
+Easy
+2.4K
+2.9K
+Companies
+Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+ 
+Example 1:
+ 
+Input: root = [1,2,3,4]
+Output: "1(2(4))(3)"
+Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+Example 2:
+ 
+Input: root = [1,2,3,null,4]
+Output: "1(2()(4))(3)"
+Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-1000 <= Node.val <= 1000
+class Solution {
+    public String tree2str(TreeNode root) {
+        StringBuilder str = new StringBuilder();
+        if(root==null) return str.toString();
+        traversal(root,str);
+        return str.toString();
+    }
+
+    static void traversal(TreeNode node,StringBuilder str)
+    {
+        if(node==null)return;
+        str.append(node.val);
+        if (node.left == null && node.right == null)
+            return;
+       
+        str.append("(");
+        traversal(node.left,str);
+        str.append(")");
+        
+        if(node.right!=null){
+            str.append("(");
+            traversal(node.right,str);
+            str.append(")");
+        }
+    }
+}
