ff77e26cb20caa1dd42a9f213827688b5deacaeb
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-28 07:50:05 +0530

    Create 603)Print linearly n to 1.java

---
+++ Recursion/603)Print linearly n to 1.java
@@ -0,0 +1,12 @@
+
+public class PrintNameNTimes {
+	public static void main(String[] args) {
+		int n=3;
+		fun(n);
+	}
+	public static void fun(int num) {
+		if(num==0)return;
+		System.out.println(num);
+		fun(num-1);
+	}
+}
