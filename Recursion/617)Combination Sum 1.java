c6995bfd4027a1494788a99e60f81d4799b6b306
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-28 11:00:43 +0530

    Create 617)Combination Sum 1.java

---
+++ Recursion/617)Combination Sum 1.java
@@ -0,0 +1,61 @@
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
+
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans =new ArrayList<>();
+        helper(0,target,candidates,ans,new ArrayList<>());
+        return ans;
+    }
+    public void helper(int curr,int target,int[] candidates,List<List<Integer>> ans,List<Integer> temp){
+        if(curr==candidates.length){
+            if(target==0)
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        if(target==0){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        if(candidates[curr]<=target){
+            temp.add(candidates[curr]);
+            helper(curr,target-candidates[curr],candidates,ans,temp);
+            temp.remove(temp.size()-1);
+        }
+        helper(curr+1,target,candidates,ans,temp);
+    }
+
+}
