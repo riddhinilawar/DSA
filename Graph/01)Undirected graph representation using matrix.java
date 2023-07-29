c50e7641c4a938e9b9732f1734bdd36d6ba9fab0
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-29 08:42:44 +0530

    Create 01)Undirected graph representation using matrix.java

---
+++ Graph/01)Undirected graph representation using matrix.java
@@ -0,0 +1,32 @@
+package graph;
+
+public class UnidirectedGraphMatrix {
+	public static void main(String[] args) {
+		
+		//input
+		int n=5,m=6;
+		int edges[][]= {{1,2},{1,3},{3,4},{2,4},{2,5},{4,5}};
+		
+		int mat[][]=new int[n+1][n+1];
+		for(int i=0;i<edges.length;i++)
+		{
+			mat[edges[i][0]][edges[i][1]]=1;
+			mat[edges[i][1]][edges[i][0]]=1;
+		}
+		
+		print(mat);
+	}
+	
+	public static void print(int mat[][])
+	{
+		for(int i=0;i<mat.length;i++)
+		{
+			for(int j=0;j<mat[0].length;j++)
+			{
+				System.out.print(mat[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
+
