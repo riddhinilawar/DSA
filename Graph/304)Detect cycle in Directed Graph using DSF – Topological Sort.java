fd536c6629ba65064e8ab13fc4262fd100cd0854
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-07-31 09:25:17 +0530

    Update 304)Detect cycle in Directed Graph using DSF – Topological Sort.java

--- Graph/304)Detect cycle in Directed Graph using DSF – Topological Sort.java
+++ Graph/304)Detect cycle in Directed Graph using DSF – Topological Sort.java
@@ -1,4 +1,4 @@
-Detect cycle in Directed Graph using DSF – Topological Sort
+Detect cycle in Directed Graph using BFS – Topological Sort
 Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, check whether it contains any cycle or not.
 
 Example 1:
