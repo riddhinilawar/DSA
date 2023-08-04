6e437b96875792e242930d89b91e2ae56a8dc9fc
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-04 09:07:14 +0530

    Create 349)797. All Paths From Source to Target-DFS.java

---
+++ Graph/349)797. All Paths From Source to Target-DFS.java
@@ -0,0 +1,41 @@
+797. All Paths From Source to Target
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+ 
+Example 1: Input: graph = [[1,2],[3],[3],[]]
+
+ 
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2: Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+
+ 
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+ 
+Constraints:
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+All the elements of graph[i] are unique.
+The input graph is guaranteed to be a DAG.
+
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ans=new ArrayList<>();
+        LinkedList<Integer> temp=new LinkedList<>();
+        temp.add(0);
+        dfs(graph,0,graph.length-1,ans,temp);
+        return ans;
+    }
+    public void dfs(int[][] graph, int start, int end, List<List<Integer>> ans, LinkedList<Integer> temp){
+        if(start==end){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int neighbours:graph[start]){
+            temp.add(neighbours);
+            dfs(graph,neighbours,graph.length-1,ans,temp);
+            temp.removeLast();
+        }
