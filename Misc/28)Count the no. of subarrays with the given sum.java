ba227f83d18dce9bb0a35e3b363bb12f651792b3
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-08 11:44:16 +0530

    Create 28)Count the no. of subarrays with the given sum.java

---
+++ Misc/28)Count the no. of subarrays with the given sum.java
@@ -0,0 +1,45 @@
+Count the no. of subarrays with the given sum, gfg,LC-560
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        int count=0;
+        int sum=0;
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        int n=nums.length;
+        
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+            
+            if(!map.isEmpty()&&map.containsKey(sum-k))
+                count+=map.get(sum-k);
+            
+            if(sum==k)
+                count+=1;
+            
+            if(map.containsKey(sum))
+                map.put(sum,map.get(sum)+1);
+            else
+                map.put(sum,1);
+        }
+        return count;
+    }
+}
+Note: Refer striver 4-5 question in striver sde sheet
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+ 
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+Constraints:
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
