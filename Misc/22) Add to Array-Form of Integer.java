adca12f6e0072f953292056a8b70869913af157c
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-08 10:09:05 +0530

    Create 22) Add to Array-Form of Integer.java

---
+++ Misc/22) Add to Array-Form of Integer.java
@@ -0,0 +1,73 @@
+989. Add to Array-Form of Integer
+
+The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+ 
+
+Example 1:
+
+Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+Example 2:
+
+Input: num = [2,7,4], k = 181
+Output: [4,5,5]
+Explanation: 274 + 181 = 455
+Example 3:
+
+Input: num = [2,1,5], k = 806
+Output: [1,0,2,1]
+Explanation: 215 + 806 = 1021
+ 
+
+Constraints:
+
+1 <= num.length <= 104
+0 <= num[i] <= 9
+num does not contain any leading zeros except for the zero itself.
+1 <= k <= 104
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+
+        int carry=0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i=num.length-1; i>=0 ;i--){
+
+            int temp=(k%10)+num[i]+carry;
+            k=k/10;
+
+            if(temp>=10){
+                carry=1;
+                temp=temp-10;
+            }
+            else carry=0;
+            ans.add(temp);
+        }
+
+        if(k>0){
+            while(k>0){
+                int temp=(k%10)+carry;
+                k=k/10;
+
+                if(temp>=10){
+                    carry=1;
+                    temp=temp-10;
+                }
+                else carry=0;
+                ans.add(temp);
+            }   
+        }
+        
+        if(carry!=0)
+            ans.add(carry);
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
