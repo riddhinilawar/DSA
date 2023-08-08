dd071edb4507b6ed3decfd0bae8a549917d1c208
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-08 14:58:56 +0530

    Create 35)Median of Two Sorted Arrays.java

---
+++ Misc/35)Median of Two Sorted Arrays.java
@@ -0,0 +1,73 @@
+4. Median of Two Sorted Arrays
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+
+
+class Solution {
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+        if(arr1.length>arr2.length)
+        return findMedianSortedArrays(arr2,arr1);
+        int m=arr1.length,n=arr2.length;
+       
+        if(m==0&&n==0)
+        {
+                return 0.0;
+        }
+        if(m==0)
+        {
+                
+                if(n%2==0)return ((double)(arr2[n/2]+arr2[(n/2)-1])/2);
+                else return (double)arr2[n/2];
+        }
+        
+        
+        
+        int low=0,high=m,medianPos=((m+n)+1)/2;
+        while(low<=high) {
+        int cut1 = (low+high)>>1;
+        int cut2 = medianPos - cut1;
+        
+        int l1 = (cut1 == 0)? Integer.MIN_VALUE:arr1[cut1-1];
+        int l2 = (cut2 == 0)? Integer.MIN_VALUE:arr2[cut2-1];
+        int r1 = (cut1 == m)? Integer.MAX_VALUE:arr1[cut1];
+        int r2 = (cut2 == n)? Integer.MAX_VALUE:arr2[cut2];
+        
+        if(l1<=r2 && l2<=r1) {
+            if((m+n)%2 != 0)
+                return Math.max(l1,l2);
+            else 
+                return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        }
+        else if(l1>r2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
+        /*[]
+          [1]*/
+    }
+}
