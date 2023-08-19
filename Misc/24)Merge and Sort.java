a92b445994f96a81f851bcf6f3ff841fa0004fbe
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-19 14:11:06 +0530

    Update 24)Merge and Sort.java

--- Misc/24)Merge and Sort.java
+++ Misc/24)Merge and Sort.java
@@ -1,4 +1,26 @@
 Merge and Sort, gfg
+
+public static int mergeNsort(int a[], int b[], int n, int m, int answer[])
+{
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++) {
+            set.add(a[i]);
+        }
+        for(int i=0; i<m; i++) {
+            set.add(b[i]);
+        }
+        
+        int i=0;
+        for(int e : set) {
+          answer[i] = e; 
+          i++;
+        }
+        
+        
+        return i;
+}
+---------------------------------------------------------------------------------------------------------
+
 class Solution {
    public static int mergeNsort(int a[], int b[], int n, int m, int answer[])
     {
