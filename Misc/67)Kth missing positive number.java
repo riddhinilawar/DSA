f08e81e2fd334ed39d4a2b45b688bb16031b469d
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-21 18:53:17 +0530

    Create 67)Kth missing positive number.java

---
+++ Misc/67)Kth missing positive number.java
@@ -0,0 +1,64 @@
+Kth missing positive number                                           LC-1539
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+Example 1:
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+Constraints:
+•	1 <= arr.length <= 1000
+•	1 <= arr[i] <= 1000
+•	1 <= k <= 1000
+•	arr[i] < arr[j] for 1 <= i < j <= arr.length
+Follow up: Could you solve this problem in less than O(n) complexity?
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        
+        int n=arr.length;
+        if(k<arr[0])
+        {
+            return k;
+        }
+        else if((arr[n-1]-n)>=k)
+        {
+            int start=0,end=n-1;
+            int mid=0;
+            while(start<=end)
+            {
+                mid=start+(end-start)/2;
+                if((arr[mid]-(mid+1))<k)
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+            }
+            
+            if(k>(arr[mid]-(mid+1)))
+            {
+                return arr[mid]+k-(arr[mid]-(mid+1));
+            }
+            else
+            {
+                mid=mid-1;
+                return arr[mid]+k-(arr[mid]-(mid+1));
+            }
+            
+        }
+        else
+        {
+            return arr[n-1]+k-(arr[n-1]-(n));
+        }
+    }
+}
