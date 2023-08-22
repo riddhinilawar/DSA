68ff9f02f3e8afec0edbf9106aab98603635fefb
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-22 17:30:48 +0530

    Create 85)Power of 2.java

---
+++ Misc/85)Power of 2.java
@@ -0,0 +1,29 @@
+Power of 2                                                                LC-231
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+ 
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+Input: n = 3
+Output: false
+ 
+Constraints:
+•	-231 <= n <= 231 - 1
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+class Solution {
+    public boolean isPowerOfTwo(int nn) {
+        long n=nn;
+        if(n==0)return false;
+        
+        if((n&(n-1))==0)return true;
+        else return false;
+    }
+}
