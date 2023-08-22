e7334380dc18b71b0f70799f6d18a3d15fe16d7d
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-22 15:01:57 +0530

    Create 518)Make Matrix Beautiful.java

---
+++ Misc1/518)Make Matrix Beautiful.java
@@ -0,0 +1,79 @@
+Make Matrix Beautiful,gfg
+
+A beautiful matrix is a matrix in which the sum of elements in each row and column is equal. Given a square matrix of size NxN. Find the minimum number of operation(s) that are required to make the matrix beautiful. In one operation you can increment the value of any one cell by 1.
+Example 1:
+
+Input:
+N = 2
+matrix[][] = {{1, 2},
+              {3, 4}}
+Output: 
+4
+Explanation:
+Updated matrix:
+4 3
+3 4
+1. Increment value of cell(0, 0) by 3
+2. Increment value of cell(0, 1) by 1
+Hence total 4 operation are required.
+Example 2:
+
+Input:
+N = 3
+matrix[][] = {{1, 2, 3},
+              {4, 2, 3},
+              {3, 2, 1}}
+Output: 
+6
+Explanation:
+Number of operations applied on each cell are as follows:
+1 2 0
+0 0 0
+0 1 2
+Such that all rows and columns have sum of 9.
+Your Task: 
+You don't need to read input or print anything. Complete the function findMinOpeartion() that takes matrix and n as input parameters and returns the minimum number of operations.
+
+Expected Time Complexity: O(N * N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= N <= 103
+1 <= matrix[i][j] <= 106
+
+
+class Solution {
+    public static int findMinOperation(int n, int[][] matrix) {
+        
+        
+        int row[]=new int[n];
+        int col[]=new int[n];
+        int maxSum=0;
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            int rowSum=0;
+            int colSum=0;
+            for(int j=0;j<n;j++){
+                rowSum+=matrix[i][j];
+                colSum+=matrix[j][i];
+            }
+            row[i]=rowSum;
+            col[i]=colSum;
+            maxSum=Math.max(maxSum,rowSum);
+            maxSum=Math.max(maxSum,colSum);
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int canAdd=maxSum-Math.max(row[i],col[j]);
+                ans+=canAdd;
+                row[i]+=canAdd;
+                col[j]+=canAdd;
+            }
+        }
+        
+        return ans;
+    }
+}
+        
