04fac88dd75bd25e00325a9aa0a5c2f2646272b7
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-22 17:33:34 +0530

    Create 87)Power of 4.java

---
+++ Misc/87)Power of 4.java
@@ -0,0 +1,25 @@
+Power of 4                                                                             LC-342
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+ 
+Example 1:
+Input: n = 16
+Output: true
+Example 2:
+Input: n = 5
+Output: false
+Example 3:
+Input: n = 1
+Output: true
+ 
+Constraints:
+•	-231 <= n <= 231 - 1
+ 
+Follow up: Could you solve it without loops/recursion?
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+class Solution {
+    public boolean isPowerOfFour(int n) {
+         return n>0 && (n & (n-1)) == 0 && (n-1)%3==0;
+    }
+}
