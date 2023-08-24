11ad54a5897d08c89a26463f42404ba32bdf885e
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-24 09:32:47 +0530

    Create Comperator.java

---
+++ Rememeber/Comperator.java
@@ -0,0 +1,12 @@
+class itemComparator implements Comparator<Item>
+{
+    @Override
+    public int compare(Item a, Item b) 
+    {
+        double r1 = (double)(a.value) / (double)(a.weight); 
+        double r2 = (double)(b.value) / (double)(b.weight); 
+        if(r1 < r2) return 1; 
+        else if(r1 > r2) return -1; 
+        else return 0; 
+    }
+}
