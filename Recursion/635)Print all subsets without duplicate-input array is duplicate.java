70a9ea07f45e1787eb1ddb9094c99674c697e6e9
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-29 11:16:36 +0530

    Update 635)Print all subsets without duplicate-input array is duplicate.java

--- Recursion/635)Print all subsets without duplicate-input array is duplicate.java
+++ Recursion/635)Print all subsets without duplicate-input array is duplicate.java
@@ -46,3 +46,27 @@ class Solution {
         helper(curr+1,set,nums,temp);
     }
 }
+
+======================================================================================================================
+
+ class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> ans =new ArrayList<>();
+        helper(0,arr,ans,new ArrayList<>());
+        return ans;
+    }
+    
+    public void helper(int idx,int[] arr,List<List<Integer>> ans,List<Integer> temp){
+        
+        ans.add(new ArrayList<>(temp));
+
+        for(int i=idx;i<arr.length;i++){
+            if(i>idx && arr[i]==arr[i-1])continue;
+            
+            temp.add(arr[i]);
+            helper(i+1,arr,ans,temp);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
