35593e4a6a534ca89057495402885e87b3124cc5
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-08-30 18:59:27 +0530

    Create 636)N-Queens II.java

---
+++ Recursion/636)N-Queens II.java
@@ -0,0 +1,56 @@
+52. N-Queens II
+TC=N!*N 
+SC=N SQUARE
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+ 
+
+Example 1:
+
+
+Input: n = 4
+Output: 2
+Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+Example 2:
+
+Input: n = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 9
+
+
+class Solution {
+    
+    public int totalNQueens(int n) {
+        int res[]=new int[1];
+        boolean upperdiag[]=new boolean[(2*n)-1];
+        boolean lowerdiag[]=new boolean[(2*n)-1];
+        boolean left[]=new boolean[n];
+        dfs(0, res,upperdiag,lowerdiag,left,n);
+        return res[0];
+    }
+
+    static void dfs(int col, int[] res,boolean upperdiag[],boolean lowerdiag[],boolean left[],int n) {
+        if (col == n) {
+            res[0]++;
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (upperdiag[row+col]==false && left[row]==false && lowerdiag[(n-1)+(col-row)]==false) {
+                upperdiag[row+col]=true;
+                left[row]=true;
+                lowerdiag[(n-1)+(col-row)]=true;
+                dfs(col + 1, res,upperdiag,lowerdiag,left,n);
+                upperdiag[row+col]=false;
+                left[row]=false;
+                lowerdiag[(n-1)+(col-row)]=false;
+            }
+        }
+    }
+}
