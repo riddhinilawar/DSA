7153736786c72088794daee865156b0ed1f8f150
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-01 07:13:40 +0530

    Update Counting Bits.java

--- Misc1/Counting Bits.java
+++ Misc1/Counting Bits.java
@@ -49,3 +49,18 @@ class Solution {
         return ans;
     }
 }
+===========================================================================================
+
+class Solution {
+    public int[] countBits(int n) {
+        int ans[]=new int[n+1];
+
+        for(int i=1;i<(n+1);i++)
+        {
+            //System.out.println(i+" "+(int)Math.pow(2,((int)(Math.log(i) / Math.log(2))))+" "+((int)(Math.log(i) / Math.log(2)))+" "+Math.log(i)+" "+Math.log(2));
+            ans[i]=1+ans[i-(int)Math.pow(2,((int)(Math.log(i) / Math.log(2))))];
+        }
+
+        return ans;
+    }
+}
