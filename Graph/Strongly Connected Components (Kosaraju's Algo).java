cda82b17ad92d9a10df58c0bbd825c1ff4fe1eeb
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-02 20:06:22 +0530

    Create Strongly Connected Components (Kosaraju's Algo).java

---
+++ Graph/Strongly Connected Components (Kosaraju's Algo).java
@@ -0,0 +1,106 @@
+Strongly Connected Components (Kosaraju's Algo)
+
+Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, Find the number of strongly connected components in the graph.
+ 
+
+Example 1:
+
+Input:
+
+Output:
+3
+Explanation:
+
+We can clearly see that there are 3 Strongly
+Connected Components in the Graph
+Example 2:
+
+Input:
+
+Output:
+1
+Explanation:
+All of the nodes are connected to each other.
+So, there's only one SCC.
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function kosaraju() which takes the number of vertices V and adjacency list of the graph of size V as inputs and returns an integer denoting the number of strongly connected components in the given graph.
+ 
+
+Expected Time Complexity: O(V+E).
+Expected Auxiliary Space: O(V+E).
+ 
+
+Constraints:
+1 ≤ V ≤ 5000
+0 ≤ E ≤ (V*(V-1))
+0 ≤ u, v ≤ V-1
+Sum of E over all testcases will not exceed 25*106
+
+
+
+class Solution
+{
+    //step 1 function-->to get toposort using dfs
+    private void dfs(int node, int []vis, ArrayList<ArrayList<Integer>> adj,
+                     Stack<Integer> st) {
+        vis[node] = 1;
+        for (Integer it : adj.get(node)) {
+            if (vis[it] == 0) {
+                dfs(it, vis, adj, st);
+            }
+        }
+        st.push(node);
+    }
+    
+    //step 3 function-->to get all the components of the graph
+    private void dfs3(int node, int[] vis, ArrayList<ArrayList<Integer>> adjT) {
+        vis[node] = 1;
+        for (Integer it : adjT.get(node)) {
+            if (vis[it] == 0) {
+                dfs3(it, vis, adjT);
+            }
+        }
+    }
+    //Function to find number of strongly connected components in the graph.
+    public int kosaraju(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        //step 1-toposort--like to get the last finished
+        int[] vis = new int[V];
+        Stack<Integer> st = new Stack<Integer>();
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                dfs(i, vis, adj, st);
+            }
+        }
+
+
+        //step 2-toposort--reverse the edges
+        ArrayList<ArrayList<Integer>> adjT = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < V; i++) {
+            adjT.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < V; i++) {
+            vis[i] = 0;
+            for (Integer it : adj.get(i)) {
+                // i -> it
+                // it -> i
+                adjT.get(it).add(i);
+            }
+        }
+        
+        //ste 3--get all the connected components
+        int scc = 0;
+        while (!st.isEmpty()) {
+            int node = st.peek();
+            st.pop();
+            if (vis[node] == 0) {
+                scc++;
+                dfs3(node, vis, adjT);
+            }
+        }
+        return scc;
+    }
+    
+}
