77c660ed010e8f7fe34a9d12b5b87603abcb04c7
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-02 20:11:58 +0530

    Create Tarjan's Algorithm(or)Critical Connections in a Network.java

---
+++ Graph/Tarjan's Algorithm(or)Critical Connections in a Network.java
@@ -0,0 +1,74 @@
+1192. Critical Connections in a Network
+
+
+
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+Output: [[1,3]]
+Explanation: [[3,1]] is also accepted.
+Example 2:
+
+Input: n = 2, connections = [[0,1]]
+Output: [[0,1]]
+ 
+
+Constraints:
+
+2 <= n <= 105
+n - 1 <= connections.length <= 105
+0 <= ai, bi <= n - 1
+ai != bi
+There are no repeated connections.
+
+
+class Solution {
+    private int timer = 1;
+    private void dfs(int node, int parent, int[] vis,
+                     ArrayList<ArrayList<Integer>> adj, int tin[], int low[],
+                     List<List<Integer>> bridges) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for (Integer it : adj.get(node)) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfs(it, node, vis, adj, tin, low, bridges);
+                low[node] = Math.min(low[node], low[it]);
+                // node --- it
+                if (low[it] > tin[node]) {
+                    bridges.add(Arrays.asList(it, node));
+                }
+            } else {
+                low[node] = Math.min(low[node], low[it]);
+            }
+        }
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        ArrayList<ArrayList<Integer>> adj =
+            new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        for (List<Integer> it : connections) {
+            int u = it.get(0); int v = it.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int[] vis = new int[n];
+        int[] tin = new int[n];
+        int[] low = new int[n];
+        List<List<Integer>> bridges = new ArrayList<>();
+        dfs(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
+    }
+}
