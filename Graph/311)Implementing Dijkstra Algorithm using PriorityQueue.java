35dcefe0a1e394035d772a67105cd22559b83441
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-05 15:59:56 +0530

    Update 311)Implementing Dijkstra Algorithm using PriorityQueue.java

--- Graph/311)Implementing Dijkstra Algorithm using PriorityQueue.java
+++ Graph/311)Implementing Dijkstra Algorithm using PriorityQueue.java
@@ -1,3 +1,11 @@
+Time Complexity : O( E log(V) ) 
+
+Where E = Number of edges and V = Number of Nodes.
+
+Space Complexity : O( |E| + |V| ) 
+
+Where E = Number of edges and V = Number of Nodes.
+
 Implementing Dijkstra Algorithm
 Given a weighted, undirected and connected graph of V vertices and an adjacency list adj where adj[i] is a list of lists containing two integers where the first integer of each list j denotes there is edge between i and j , second integers corresponds to the weight of that  edge . You are given the source vertex S and You to Find the shortest distance of all the vertex's from the source vertex S. You have to return a list of integers denoting shortest distance between each node and Source vertex S.
  
