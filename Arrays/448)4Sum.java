8678013e421db2330de9bf2e08a0cc14acc53d3b
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-06 06:38:36 +0530

    Update 448)4Sum.java

--- Arrays/448)4Sum.java
+++ Arrays/448)4Sum.java
@@ -55,3 +55,79 @@ class Solution {
         return list;
     }
 }
+
+==============================================================================================================================================
+ TC:-o(N square)
+
+ // Java program for the above approach
+import java.util.*;
+ 
+class GFG{
+ 
+// Function to return the number of
+// quadruplets having the given sum
+static int countSum(int a[], int n, int sum)
+{
+     
+    // Initialize variables
+    int i, j, k;
+ 
+    // Initialize answer
+    int count = 0;
+ 
+    // Store the frequency of sum
+    // of first two elements
+    HashMap<Integer, Integer> m = new HashMap<>();
+ 
+    // Traverse from 0 to N-1, where
+    // arr[i] is the 3rd element
+    for(i = 0; i < n - 1; i++)
+    {
+         
+        // All possible 4th elements
+        for(j = i + 1; j < n; j++)
+        {
+             
+            // Sum of last two element
+            int temp = a[i] + a[j];
+ 
+            // Frequency of sum of first
+            // two elements
+            if (temp < sum && m.containsKey(sum - temp))
+                count += m.get(sum - temp);
+        }
+        for(j = 0; j < i; j++)
+        {
+             
+            // Store frequency of all possible
+            // sums of first two elements
+            int temp = a[i] + a[j];
+ 
+            if (temp < sum)
+                if (m.containsKey(temp))
+                    m.put(temp, m.get(temp) + 1);
+                else
+                    m.put(temp, 1);
+        }
+    }
+     
+    // Return the answer
+    return count;
+}
+ 
+// Driver Code
+public static void main(String[] args)
+{
+     
+    // Given array arr[]
+    int arr[] = { 4, 5, 3, 1, 2, 4 };
+ 
+    // Given sum S
+    int S = 13;
+ 
+    int N = arr.length;
+ 
+    // Function Call
+    System.out.print(countSum(arr, N, S));
+}
+}
