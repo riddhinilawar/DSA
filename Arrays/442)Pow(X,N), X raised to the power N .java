dc74bddd2d380f7ceb2b56f894611a79467de2fb
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-09 17:20:04 +0530

    Update 442)Pow(X,N), X raised to the power N .java

--- Arrays/442)Pow(X,N), X raised to the power N .java
+++ Arrays/442)Pow(X,N), X raised to the power N .java
@@ -3,6 +3,9 @@ Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 Example 1:
 Input: x = 2.00000, n = 10
 Output: 1024.00000
+
+(((2 * 2)power 2)power2)
+	
 Example 2:
 Input: x = 2.10000, n = 3
 Output: 9.26100
