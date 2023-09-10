8f75a190a4aed0a9c5536fd45f4aaa4407d8b985
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-10 20:03:55 +0530

    Update Comperator.java

--- Rememeber/Comperator.java
+++ Rememeber/Comperator.java
@@ -41,3 +41,33 @@ class ItemComparator implements Comparator<Pair>{
 }
 
  PriorityQueue<Pair> pq = new PriorityQueue<>(new ItemComparator());
+
+-----------------------------------------------------------------------------------------------------------------------------------
+With Collection.sort:
+
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+    Edge(int _src, int _dest, int _wt) {
+        this.src = _src; this.dest = _dest; this.weight = _wt;
+    }
+    // Comparator function used for
+    // sorting edgesbased on their weight
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+};
+
+ Collections.sort(edges);
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+    Edge(int _src, int _dest, int _wt) {
+        this.src = _src; this.dest = _dest; this.weight = _wt;
+    }
+    // Comparator function used for
+    // sorting edgesbased on their weight
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+};
