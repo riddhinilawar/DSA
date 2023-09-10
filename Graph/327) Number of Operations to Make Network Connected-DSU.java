e356ebc0f92a960accb6ffd054def3fe9e690800
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-10 20:37:10 +0530

    Create 327) Number of Operations to Make Network Connected-DSU.java

---
+++ Graph/327) Number of Operations to Make Network Connected-DSU.java
@@ -0,0 +1,130 @@
+1319. Number of Operations to Make Network Connected
+
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+Example 2:
+
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+ 
+
+Constraints:
+
+1 <= n <= 105
+1 <= connections.length <= min(n * (n - 1) / 2, 105)
+connections[i].length == 2
+0 <= ai, bi < n
+ai != bi
+There are no repeated connections.
+No two computers are connected by more than one cable.
+
+
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet dsuObj = new DisjointSet(n);
+
+        int connectionsCount=connections.length;
+
+        for(int i=0;i<connectionsCount;i++){
+            int pu=dsuObj.findUPar(connections[i][0]);
+            int pv=dsuObj.findUPar(connections[i][1]);
+
+            if(pu==pv)continue;
+            dsuObj.unionBySize(connections[i][0],connections[i][1]);
+        }
+
+        int totalComponenets=0;
+
+        for(int i=0;i<n;i++){
+            if(dsuObj.parent.get(i)==i)
+                totalComponenets++;
+        }
+
+        DisjointSet dsuObj2 = new DisjointSet(n);
+        
+        int extraEdges=0;
+
+        for(int i=0;i<connectionsCount;i++){
+            int pu=dsuObj2.findUPar(connections[i][0]);
+            int pv=dsuObj2.findUPar(connections[i][1]);
+
+            if(pu==pv)extraEdges++;;
+            dsuObj2.unionBySize(connections[i][0],connections[i][1]);
+        }
+
+        if(totalComponenets-1<=extraEdges){
+            return totalComponenets-1;
+        }
+        else{
+            return -1;
+        }
+    }
+}
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
