850b32c824133bce73e283c0a3132292f49a4e0e
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-10 20:14:40 +0530

    Update 325)Kruskal's Algo Minimum Spanning Tree.java

--- Graph/325)Kruskal's Algo Minimum Spanning Tree.java
+++ Graph/325)Kruskal's Algo Minimum Spanning Tree.java
@@ -1,6 +1,5 @@
 Given a weighted, undirected and connected graph of V vertices and E edges. The task is to find the sum of weights of the edges of the Minimum Spanning Tree. Given  adjacency list adj as input parameters . Here adj[i] contains vectors of size 2, where the first integer in that vector denotes the end of the edge and the second integer denotes the edge weight.
 
- krushkals
 
 Example 1:
 
