3aa2b247f4e3b2005c25e5554848fd2828315b84
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-09-17 05:36:13 +0530

    Create Greatest Common Divisor of Strings.java

---
+++ Misc1/Greatest Common Divisor of Strings.java
@@ -0,0 +1,43 @@
+1071. Greatest Common Divisor of Strings
+
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        int n1 = str1.length(), n2 = str2.length();
+        for (int i = Math.min(n1, n2); i >= 1; i--) {
+            String x = str1.substring(0, i);
+            if (check(x, str1) && check(x, str2))
+                return x;
+        }
+        return "";
+    }
+
+    private boolean check(String t, String s) {
+        int n = s.length() / t.length();
+        return t.repeat(n).equals(s);
+    }
+}
