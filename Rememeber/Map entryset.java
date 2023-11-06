1b6340a00b19450287a86db3f2e98472f41f0f40
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2023-11-06 07:54:04 +0530

    Update Map entryset.java

--- Rememeber/Map entryset.java
+++ Rememeber/Map entryset.java
@@ -1,2 +1,8 @@
  for(Map.Entry<Integer,Integer> entry:map.entrySet())
             ans.add(entry.getValue());
+
+TreeMap lowerKey()
+numMap.lowerKey(7);
+
+TreeMap higherKey()
+treemap.higherKey(3);
