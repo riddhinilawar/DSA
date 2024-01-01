2a3a76e11eaf6592ca61aa6e93ef4c4e73200ce2
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-01-01 17:00:21 +0530

    Update 5)Max Consecutive once.java

--- Misc/5)Max Consecutive once.java
+++ Misc/5)Max Consecutive once.java
@@ -3,35 +3,26 @@ class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int ans=1;
         int count=1;
-        //handling exception when their is only 1 elemet in the array
-        if(nums.length==1)
-        {
+        int count0=1;
+        if(nums.length==1){
             if(nums[0]==0)return 0;
             else return 1;
         }
         //handing exception if all elements in array are zero.
-        int count0=1;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            if(nums[i]==nums[i+1]&&nums[i]==1)
-            {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]&&nums[i]==1){
                 count++;
                 ans=Math.max(count,ans);
             }
-            else
-            {
+            else{
                 count=1;
-            }   
+            }
             //handling exception
-            if(nums[i]==nums[i+1]&&nums[i]==0)
-            {
+            if(nums[i+1]==0){
                 count0++;
             }
         }
-        if(count0==nums.length)return 0;
-        else
-        return ans;
-        /*[1]*/
+        return (count0==nums.length)? 0:ans;
     }
 }
 Given a binary array nums, return the maximum number of consecutive 1's in the array.
