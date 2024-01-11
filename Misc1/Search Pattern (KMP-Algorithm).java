08a4ba66e6bc8284b8dac1b6df29a36c96739e2d
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-01-11 13:14:30 +0530

    Update Search Pattern (KMP-Algorithm).java

--- Misc1/Search Pattern (KMP-Algorithm).java
+++ Misc1/Search Pattern (KMP-Algorithm).java
@@ -31,3 +31,82 @@ Constraints:
 Both the strings consists of lowercase English alphabets.
 
 
+  class Solution{
+      ArrayList<Integer> search(String pat, String txt){
+        ArrayList<Integer> list = new ArrayList<>();
+        int index = txt.indexOf(pat);
+        while(index != -1){
+            list.add(index+1);
+            index=txt.indexOf(pat,index+1);
+        }
+        return list;
+    }
+}
+Note: IndexOf(String, Int32, StringComparison) Reports the zero-based index of the first occurrence of the specified string in the current String object. 
+==============================================================================================
+https://www.youtube.com/watch?v=GTJr8OvyEVQ
+
+class Solution{
+    ArrayList<Integer> search(String pat, String txt){
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        
+        //getting the preprocessed values in dp
+        int dp[]=new int[pat.length()];
+        dp[0]=0;
+        
+        int i=0;
+        for(int j=1;j<pat.length();j++){
+            if(pat.charAt(i)==pat.charAt(j)){
+                dp[j]=i+1;
+                i++;
+            }
+            else{
+                while(pat.charAt(i)!=pat.charAt(j)){
+                    
+                    if(i==0){
+                        dp[j]=0;
+                        break;
+                    }
+                    
+                    i=dp[i-1];
+                    
+                    if(pat.charAt(i)==pat.charAt(j)){
+                        dp[j]=i+1;
+                        i++;
+                        break;
+                    }
+                }
+            }
+        }
+        // for(int num:dp){
+        //     System.out.println(num);
+        // }
+        
+        
+        i=0;
+        int j=0;
+        
+        while(i<txt.length()){
+            if(txt.charAt(i)==pat.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j==pat.length()){
+                ans.add(i-j+1);
+                j=dp[j-1];
+            }
+            else if(i<txt.length() && txt.charAt(i) != pat.charAt(j)){
+                if(j!=0){
+                    j=dp[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        
+    return ans;
+    }
+}
+
