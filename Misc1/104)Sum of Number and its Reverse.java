467162c58e469de2688475e9039a63c5d258d188
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-01-23 17:55:42 +0530

    Update 104)Sum of Number and its Reverse.java

--- Misc1/104)Sum of Number and its Reverse.java
+++ Misc1/104)Sum of Number and its Reverse.java
@@ -20,22 +20,73 @@ Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
 class Solution {
     public boolean sumOfNumberAndReverse(int num) {
-        if(num==0)return true;
-        int start=0;
-        int end=num;
-
-        for(int i=0;i<num;i++)
+        for(int i=0;i<=num;i++)
             if(i+rev(i)==num)return true;
         return false;
     }
-    public int rev(int n)
-    {
+    public int rev(int n){
         int temp=0;
-        while(n>0)
-        {
+        while(n>0){
             temp = temp*10 + n%10;
             n/=10;
         }
         return temp;
+    }
+}
+==================================================================================
+ class Solution {
+    public boolean sumOfNumberAndReverse(int num) {
+        //a,a => 2a
+        if(num<=18) {
+            if(num%2==0) {
+                return true;
+            }
+        }
+        //a,a=>2a
+        
+        //ab, ba => 11(a+b)
+        if(num>=11) {
+            if(num%11==0 && num/11<=18) {
+             //   System.out.println(num);
+                return true;
+            }
+        }
+        //abc, cba => 101(a+c) + 20b
+        if(num>=101) {
+            for(int i=1;i<=18;i++) {
+                int tmp = num-101*i;
+                if(tmp<0) break;
+                if(tmp%20==0&& tmp/20<=9) {
+               //     System.out.println(i+" "+tmp);
+                    return true;
+                }
+            }
+        }
+        //abcd, dcba => 1001(a+d)+110(b+c)
+        if(num>=1001) {
+            for(int i=1;i<=18;i++) {
+                int tmp = num-1001*i;
+                if(tmp<0) break;
+                if(tmp%110==0&& tmp/110<=18) {
+                   // System.out.println(i+"  "+tmp);
+                    return true;
+                }
+            }            
+        }
+        
+        //abcde, edcba => 10001(a+e)+ 1010(b+d) +200c
+        if(num>=10001) {
+            for(int i=1;i<=18;i++) {
+                for(int j=0;j<=18;j++) {
+                    int tmp = num-10001*i-1010*j;
+                    if(tmp<0) break;
+                    if(tmp%200==0&& tmp/200<=9) {
+                       //   System.out.println(i+" "+j+" "+tmp);
+                        return true;
+                    }
+                }
+            }              
+        }
+        return false;
     }
 }
