1d72d6fe6ed3657a47fce65843c07499ee03d710
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-01-26 15:53:15 +0530

    Update Random.java

--- Rememeber/Random.java
+++ Rememeber/Random.java
@@ -44,3 +44,22 @@ class GenerateRandom {
       System.out.println("Random double value between 0.0 and 1.0 : " + double_random);
     }
 }
+
+
+---------------------------------------------------------------------------
+reverse sampling --> if(Math.random() <= (1.0/scope))
+
+public int getRandom() {
+    int scope=1, number=0;
+    ListNode curr=this.head;
+    while(curr!=null){
+        if(Math.random() <= (1.0/scope))
+            number=curr.val;
+        scope++;
+        curr=curr.next;
+    }
+    return number;
+}
+------------------------------------------------------------------------------------
+
+    
