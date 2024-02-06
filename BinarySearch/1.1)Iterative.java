f197c2262f66e2d0f0756bb2b0f84be6d9a704f2
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-06 15:46:17 +0530

    Update 1.1)Iterative.java

--- BinarySearch/1.1)Iterative.java
+++ BinarySearch/1.1)Iterative.java
@@ -15,3 +15,12 @@ public void binarySearch(int arr[],int n,int target){
     }
     return -1;
 }
+
+TC:As we divide : O(log n) 
+    Ex: n=32
+    log base2 32
+    log base2 2power5
+    5 log base2 2
+    5*1
+    5
+    for the array of size 32 we need 5 operations to perform and get the target element.
