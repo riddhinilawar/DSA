85e54bab9e5d3317f8a004bbbe1ec0133d6fe456
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-06 18:07:19 +0530

    Create 6)Find the Minimum element in a Sorted and Rotated Array.java

---
+++ BinarySearch/6)Find the Minimum element in a Sorted and Rotated Array.java
@@ -0,0 +1,12 @@
+Given an array arr[] which is sorted and rotated, the task is to find an element in the rotated array (with duplicates) in O(log n) time. 
+Note: Print the index where the key exists. In case of multiple answer print any of them
+
+Examples: 
+
+Input: arr[] = {3, 3, 3, 1, 2, 3}, key = 3 
+Output: 0 
+arr[0] = 3
+
+Input: arr[] = {3, 3, 3, 1, 2, 3}, key = 11 
+Output: -1 
+11 is not present in the given array. 
