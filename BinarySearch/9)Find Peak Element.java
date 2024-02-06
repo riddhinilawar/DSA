b46c875fb96ee09b2c97e72b9594c83e77685880
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-06 18:42:31 +0530

    Update 9)Find Peak Element.java

--- BinarySearch/9)Find Peak Element.java
+++ BinarySearch/9)Find Peak Element.java
@@ -1,3 +1,5 @@
+Find peak element,gfg                     LC-162     
+
 class Solution {
     public int findPeakElement(int[] nums) {
         int n=nums.length;
@@ -24,7 +26,7 @@ class Solution {
 }
 
 
-Find peak element,gfg                     LC-162     
+
 A peak element is an element that is strictly greater than its neighbors.
 Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
 You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.   You must write an algorithm that runs in O(log n) time.
