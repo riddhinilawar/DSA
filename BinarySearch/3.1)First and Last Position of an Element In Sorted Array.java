6915c4f48421b08e08dcb03d409314061df61274
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-06 17:30:31 +0530

    Update 3.1)First and Last Position of an Element In Sorted Array.java

--- BinarySearch/3.1)First and Last Position of an Element In Sorted Array.java
+++ BinarySearch/3.1)First and Last Position of an Element In Sorted Array.java
@@ -1,4 +1,108 @@
+Approach 1)Using upper bund lower bound method: first occ:lower bound, last occ:upper bound -1
 
+import java.util.*;
+public class Solution {
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        int ans[]=new int[2];
+        int lb=lowerBound(arr,n,k);
+        if(arr.get(lb)!=k){
+            ans[0]=-1;
+            ans[1]=-1;
+            return ans;
+        }
+        ans[0]=lb;
+        ans[1]=upperBound(arr,n,k)-1;
+        return ans;
+    }
+    public static int lowerBound(ArrayList<Integer> arr, int n, int x) {
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static int upperBound(ArrayList<Integer> arr, int n, int x){
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return (ans==-1)?n:ans;
+    }
+};
+
+
+=====================================================================================================================
+
+Approach 2)Using simple binary search
+class Solution {
+    
+    public pair indexes(long v[], long x){
+        int first=firstOcc(v,v.length,x);
+        if(first == -1 || v[first]!=x){
+            return new pair(-1,-1);
+        }
+        int last=lastOcc(v,v.length,x);
+        return new pair(first,last);
+    }
+    public static int firstOcc(long v[], int n, long x) {
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(v[mid]==x){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(v[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static int lastOcc(long v[], int n, long x){
+        int low=0;
+        int high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            //System.out.println(mid);
+            if(v[mid]==x){
+                ans=mid;
+                low=mid+1;
+            }
+            else if(v[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
 
 
 Problem statement
