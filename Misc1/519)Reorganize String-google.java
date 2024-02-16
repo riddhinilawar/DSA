466f605bef40838437724d96f83c1cdefd328382
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-16 12:17:37 +0530

    Update 519)Reorganize String-google.java

--- Misc1/519)Reorganize String-google.java
+++ Misc1/519)Reorganize String-google.java
@@ -1,5 +1,5 @@
 767. Reorganize String
-
+Node:- Need to find the max freq and check weather it can be kept in alternating way in a string and also others will be managed in the remaining positions.
 Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
 
 Return any possible rearrangement of s or return "" if not possible.
@@ -20,8 +20,47 @@ Constraints:
 
 1 <= s.length <= 500
 s consists of lowercase English letters.
+class Solution {
+    public String reorganizeString(String s) {
+        int n=s.length();
+        int freq[]=new int[26];
+        int maxFreq=0;
+        char maxChar='-';
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+            if(freq[c-'a']>maxFreq){
+                maxFreq=freq[c-'a'];
+                maxChar=c;
+            }
+        }
 
+        if(n%2==0 && maxFreq>(n/2))return "";
+        if(n%2==1 && maxFreq>(n+1)/2)return "";
 
+        char ans[]=new char[s.length()];
+        
+        int idx=0;
+        while(idx<n && freq[maxChar-'a']>0){
+            ans[idx]=maxChar;
+            idx+=2;
+            freq[maxChar-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            while(freq[i]>0){
+                if(idx>=n){
+                    idx=1;
+                }
+                ans[idx]=(char)(i+'a');
+                idx+=2;
+                freq[i]--;
+            }
+        }
+
+        return String.valueOf(ans);
+    }
+}
+=========================================================================================================
 class Solution {
     public String reorganizeString(String s) {
         int freq[]=new int[26];
