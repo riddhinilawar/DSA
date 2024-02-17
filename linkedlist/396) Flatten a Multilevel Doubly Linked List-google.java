6c82dacad79c3a4147298f8f5ba9f3a5bbce9051
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-17 14:24:11 +0530

    Update 396) Flatten a Multilevel Doubly Linked List-google.java

--- linkedlist/396) Flatten a Multilevel Doubly Linked List-google.java
+++ linkedlist/396) Flatten a Multilevel Doubly Linked List-google.java
@@ -32,3 +32,45 @@ class Solution {
         return head;
     }
 }
+
+
+=============================================using stack========================================
+
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Stack<Node> stack=new Stack<>();
+
+        Node curr=head;
+        while(curr!=null){
+            if(curr.child!=null){
+                if(curr.next!=null){
+                    stack.push(curr.next);
+                }
+                curr.next=curr.child;
+                curr.child.prev=curr;
+                curr.child=null;
+            }
+
+            if(curr.next==null && !stack.isEmpty()){
+                Node temp=stack.pop();
+                curr.next=temp;
+                temp.prev=curr;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+
+        return head;
+    }
+}
