c04e65589ba23c5d548a492ecb69889014841efe
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-19 12:12:35 +0530

    Create Find the middle occurrence of X in the given array.java

---
+++ BinarySearch/Find the middle occurrence of X in the given array.java
@@ -0,0 +1,10 @@
+Q1. Find the middle occurrence of X in the given array
+A = [1,2,2,2,2,3,4,5,6,7] , X= 2
+Output : 2
+
+
+Solution::
+  
+  step 1)Find the first occurance index
+  step2) find the last occuring index
+  step 3) return (first occurance+last occurance)/2
