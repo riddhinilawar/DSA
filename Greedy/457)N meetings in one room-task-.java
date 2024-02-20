412465be74ddc65a9838bc65fdd0041f082a9342
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-20 11:29:24 +0530

    Update and rename 457)N meetings in one room.java to 457)N meetings in one room-task-.java

--- Greedy/457)N meetings in one room-task-.java
+++ Greedy/457)N meetings in one room-task-.java
@@ -1,3 +1,6 @@
+Note::sort the meeting by end time...because we need to attand the more meeting..faster the meeting ends...next meeting held scope will improve
+
+
 There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
 What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
 
