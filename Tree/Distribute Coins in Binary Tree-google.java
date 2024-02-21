eb41e2915bde899956b25f3f23112aff8de1063c
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-21 17:49:41 +0530

    Create Distribute Coins in Binary Tree-google.java

---
+++ Tree/Distribute Coins in Binary Tree-google.java
@@ -0,0 +1,63 @@
+979. Distribute Coins in Binary Tree
+Note::Simple post order traversal..hint is..just sum the return values
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+
+Return the minimum number of moves required to make every node have exactly one coin.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,0,0]
+Output: 2
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+Example 2:
+
+
+Input: root = [0,3,0]
+Output: 3
+Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= n <= 100
+0 <= Node.val <= n
+The sum of all Node.val is n.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int moves;
+    public int distributeCoins(TreeNode root) {
+        moves=0;
+        helper(root);
+        return moves;
+    }
+    public int helper(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=helper(root.left);
+        int right=helper(root.right);
+        moves+=Math.abs(left)+Math.abs(right);
+        return root.val+left+right-1;
+    }
+}
