cdc1f783fc505ff2f01ac0c0538dfd23aaa17753
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-02-21 17:11:27 +0530

    Create Greater than pair - Google.java

---
+++ Graph/Greater than pair - Google.java
@@ -0,0 +1,7 @@
+https://leetcode.com/discuss/interview-question/4220437/Google-or-Phone-interview
+
+Given a list of "greater than" pairs, return a tuple with:
+-> If there is a single sorted order, the minimum index i such that the first i + 1 pairs are sufficient to find the order.
+
+Input : a > b, b > c, a > c
+Output: 1
