ee4c549880bf343a3ba1e255caa93d111fbaa9b5
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-13 21:38:41 +0530

    Update and rename Minimize the Maximum Difference of PairsMinimize the Maximum Difference of Pairs.java to Minimize the Maximum Difference of Pairs.java

--- BinarySearch/Minimize the Maximum Difference of Pairs.java
+++ BinarySearch/Minimize the Maximum Difference of Pairs.java
@@ -28,3 +28,37 @@ Constraints:
 1 <= nums.length <= 105
 0 <= nums[i] <= 109
 0 <= p <= (nums.length)/2
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+        int ans=0;
+        while (left <= right) {
+            int midDiff = (left + right) / 2;
+            if (isFeasible(midDiff, nums, p)) {
+                ans=midDiff;
+                right = midDiff-1;
+            } else {
+                left = midDiff + 1;
+            }
+        }
+
+        return ans;        
+    }
+
+    private boolean isFeasible(int diff, int[] nums, int p) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.length - 1 && count < p) {
+            if (nums[i + 1] - nums[i] <= diff) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return count >= p;
+    }    
+}
