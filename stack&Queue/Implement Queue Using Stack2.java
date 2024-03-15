b4c4129253d9e3e0636609cf456345eaf54d42ab
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-15 09:46:11 +0530

    Update Implement Queue Using Stack2.java

--- stack&Queue/Implement Queue Using Stack2.java
+++ stack&Queue/Implement Queue Using Stack2.java
@@ -79,3 +79,44 @@ public class ImplementQueueUsingStack2 {
 		
 	}
 }
+
+
+=========================================================================================
+
+public class QueueUsingSingleStack {
+ 
+    Stack<Integer> stack = new Stack<>();
+ 
+    private void enqueue(int i) {
+        stack.push(i);
+    }
+ 
+    private int dequeue() throws Exception {
+        if (stack.size() == 0)
+            throw new Exception("Queue is Empty");
+ 
+        if (stack.size() == 1)
+            return stack.pop();
+ 
+        int data = stack.pop();
+        int retVal = dequeue();
+        stack.push(data);
+        return retVal;
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        QueueUsingSingleStack queue = new QueueUsingSingleStack();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+ 
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+ 
+    }
+ 
+}
+has context menu
