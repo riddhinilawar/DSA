e57a12fd0817509c45d6b27cf8b3b2be794f1ff1
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-19 13:09:06 +0530

    Update 320)Floyd Warshall.java

--- Graph/320)Floyd Warshall.java
+++ Graph/320)Floyd Warshall.java
@@ -1,5 +1,6 @@
 Floyd Warshall
-
+the Floyd-Warshall algorithm does support negative weight edges in a directed graph so long the weighted sum of such edges forming a cycle isn't negative
+ 
 The problem is to find the shortest distances between every pair of vertices in a given edge-weighted directed graph. The graph is represented as an adjacency matrix of size n*n. Matrix[i][j] denotes the weight of the edge from i to j. If Matrix[i][j]=-1, it means there is no edge from i to j.
 Do it in-place.
 Example 1:
