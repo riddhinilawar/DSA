dde2208060f09570b2d64edbb1133f1950b3c9c5
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-22 19:44:41 +0530

    Update Map entryset.java

--- Rememeber/Map entryset.java
+++ Rememeber/Map entryset.java
@@ -6,3 +6,21 @@ numMap.lowerKey(7);
 
 TreeMap higherKey()
 treemap.higherKey(3);
+
+
+
+  public void assign(K keyBegin, K keyEnd, V val) {
+        if (keyBegin.compareTo(keyEnd) >= 0) {
+            return; // Empty interval, do nothing
+        }
+
+        // Remove existing mappings in the interval
+        map.subMap(keyBegin, keyEnd).clear();
+        map.put(keyBegin, val);
+        map.put(keyEnd, map.getOrDefault(keyEnd, valBegin));
+    }
+
+    public V get(K key) {
+        Map.Entry<K, V> entry = map.floorEntry(key);
+        return entry != null ? entry.getValue() : valBegin;
+    }
