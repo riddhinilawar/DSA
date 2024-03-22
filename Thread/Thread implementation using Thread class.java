5cf9ff3e97add7d0a390c143266e9226d394c4c6
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-22 18:49:39 +0530

    Create Thread implementation using Thread class.java

---
+++ Thread/Thread implementation using Thread class.java
@@ -0,0 +1,26 @@
+package design;
+
+public class Solution2 {
+	public static void main(String[] args) {
+		ThreadExample thread1 = new ThreadExample();
+		thread1.start();
+		
+	}
+}
+class ThreadExample extends Thread{
+	int count=0;
+	
+	public void run() {
+		System.out.println("Thread started");
+		try{
+			while(count<5) {
+				Thread.sleep(500);
+				System.out.println("In thread, ount is::"+count);
+				count++;
+			}
+		}
+		catch(InterruptedException e){
+			System.out.println("Thread Interrupted");
+		}
+	}
+}
