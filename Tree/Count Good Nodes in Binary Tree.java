678f4a02709eb90109a38de6df9e1b29c0b74a46
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-03-27 14:09:16 +0530

    Update Count Good Nodes in Binary Tree.java

--- Tree/Count Good Nodes in Binary Tree.java
+++ Tree/Count Good Nodes in Binary Tree.java
@@ -1,9 +1,5 @@
 1448. Count Good Nodes in Binary Tree
-Solved
-Medium
-Topics
-Companies
-Hint
+
 Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
 
 Return the number of good nodes in the binary tree.
