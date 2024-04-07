75b3a65a2c24291b3fd285299db8035c5ca01d71
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-04-07 07:20:17 +0530

    Create Count Unreachable Pairs of Nodes in an Undirected Graph-google39.java

---
+++ Graph/Count Unreachable Pairs of Nodes in an Undirected Graph-google39.java
@@ -0,0 +1,109 @@
+2316. Count Unreachable Pairs of Nodes in an Undirected Graph
+
+You are given an integer n. There is an undirected graph with n nodes, numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting nodes ai and bi.
+
+Return the number of pairs of different nodes that are unreachable from each other.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, edges = [[0,1],[0,2],[1,2]]
+Output: 0
+Explanation: There are no pairs of nodes that are unreachable from each other. Therefore, we return 0.
+Example 2:
+
+
+Input: n = 7, edges = [[0,2],[0,5],[2,4],[1,6],[5,4]]
+Output: 14
+Explanation: There are 14 pairs of nodes that are unreachable from each other:
+[[0,1],[0,3],[0,6],[1,2],[1,3],[1,4],[1,5],[2,3],[2,6],[3,4],[3,5],[3,6],[4,6],[5,6]].
+Therefore, we return 14.
+ 
+
+Constraints:
+
+1 <= n <= 105
+0 <= edges.length <= 2 * 105
+edges[i].length == 2
+0 <= ai, bi < n
+ai != bi
+There are no repeated edges.
+
+
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        DisjointSet obj = new DisjointSet(n);
+        for(int edge[]:edges){
+            obj.unionBySize(edge[0],edge[1]);
+        }
+
+        long sum=0;
+        long ans=0;
+        ArrayList<Long> list = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            if(obj.parent.get(i)==i){
+                list.add((long)obj.size.get(i));
+                sum+=(long)obj.size.get(i);
+            }
+        }
+        //System.out.println(list);
+        for(int i=0;i<list.size();i++){
+            sum-=list.get(i);
+            ans+=(list.get(i)*sum);
+        }
+
+        return ans;
+    }
+}
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
