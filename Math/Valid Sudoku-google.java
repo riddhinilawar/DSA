13f6bfd2d0c9d8763ac8db798cb1e56ffe5f844d
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-04-20 08:02:38 +0530

    Update and rename Valid Sudoku.java to Valid Sudoku-google.java

--- Math/Valid Sudoku-google.java
+++ Math/Valid Sudoku-google.java
@@ -52,13 +52,14 @@ class Solution {
     }
     public boolean solve(char[][] board){
         for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[0].length;j++){
+            for(int j=0;j<board[0].length;j++){
 
-            if(board[i][j]!='.'){
-                if(isValid(board[i][j],i,j,board)==false)
-                    return false;
+                if(board[i][j]!='.'){
+                    if(isValid(board[i][j],i,j,board)==false)
+                        return false;
+                }
             }
-        }}
+        }
         return true;
     }
     public boolean isValid(char c,int row,int column,char[][] board){
