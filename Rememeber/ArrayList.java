2af0339175b42efe4892b49bf25903b2928ea569
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-04-23 09:19:12 +0530

    Update ArrayList.java

--- Rememeber/ArrayList.java
+++ Rememeber/ArrayList.java
@@ -1,6 +1,11 @@
 //to compare 2 ArrayList
 arr1.equals(arr2);
 
+//addAll:: to all all the elements of list 2 in list 1
+lis1.addAll(list2); 
+
+//for edgecases when need to return 1 integer in list
+if(n == 1) return Collections.singletonList(0);
 
 //to check if HashSet contains arraylist
 if(list.contains(temp)==false)
