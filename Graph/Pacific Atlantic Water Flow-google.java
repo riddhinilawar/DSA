a3bad491aaaa1a09b978d94b3e752d7e330ec642
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-04-24 07:57:33 +0530

    Create Pacific Atlantic Water Flow-google.java

---
+++ Graph/Pacific Atlantic Water Flow-google.java
@@ -0,0 +1,107 @@
+417. Pacific Atlantic Water Flow
+
+There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+
+The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+ 
+
+Example 1:
+
+
+Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+Explanation: The following cells can flow to the Pacific and Atlantic oceans, as shown below:
+[0,4]: [0,4] -> Pacific Ocean 
+       [0,4] -> Atlantic Ocean
+[1,3]: [1,3] -> [0,3] -> Pacific Ocean 
+       [1,3] -> [1,4] -> Atlantic Ocean
+[1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean 
+       [1,4] -> Atlantic Ocean
+[2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean 
+       [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+[3,0]: [3,0] -> Pacific Ocean 
+       [3,0] -> [4,0] -> Atlantic Ocean
+[3,1]: [3,1] -> [3,0] -> Pacific Ocean 
+       [3,1] -> [4,1] -> Atlantic Ocean
+[4,0]: [4,0] -> Pacific Ocean 
+       [4,0] -> Atlantic Ocean
+Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans.
+Example 2:
+
+Input: heights = [[1]]
+Output: [[0,0]]
+Explanation: The water can flow from the only cell to the Pacific and Atlantic oceans.
+ 
+
+Constraints:
+
+m == heights.length
+n == heights[r].length
+1 <= m, n <= 200
+0 <= heights[r][c] <= 105
+
+class Solution {
+    int[][]dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        
+        List<List<Integer>> res = new LinkedList<>();
+
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return res;
+        }
+
+        int n = matrix.length, m = matrix[0].length;
+        //One visited map for each ocean
+        boolean[][] pacific = new boolean[n][m];
+        boolean[][] atlantic = new boolean[n][m];
+
+        Queue<int[]> pQueue = new LinkedList<>();
+        Queue<int[]> aQueue = new LinkedList<>();
+
+        for(int i=0; i<n; i++){ //Vertical border
+            pQueue.offer(new int[]{i, 0});
+            aQueue.offer(new int[]{i, m-1});
+            pacific[i][0] = true;
+            atlantic[i][m-1] = true;
+        }
+
+        for(int i=0; i<m; i++){ //Horizontal border
+            pQueue.offer(new int[]{0, i});
+            aQueue.offer(new int[]{n-1, i});
+            pacific[0][i] = true;
+            atlantic[n-1][i] = true;
+        }
+
+        bfs(matrix, pQueue, pacific);
+        bfs(matrix, aQueue, atlantic);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(pacific[i][j] && atlantic[i][j])
+                    res.add(Arrays.asList(i, j));
+            }
+        }
+
+        return res;
+    }
+    public void bfs(int[][]matrix, Queue<int[]> queue, boolean[][]visited){
+        int n = matrix.length, m = matrix[0].length;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            for(int[] d:dir){
+                int x = cur[0]+d[0];
+                int y = cur[1]+d[1];
+                if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < matrix[cur[0]][cur[1]]){
+                    continue;
+                }
+                visited[x][y] = true;
+                queue.offer(new int[]{x, y});
+            } 
+        }
+    }
+}
