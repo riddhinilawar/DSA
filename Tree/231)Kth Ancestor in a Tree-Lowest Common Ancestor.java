a41f8b986f08c82e085afe826307b675cbeb397e
riddhinilawar <62386378+riddhinilawar@users.noreply.github.com>
2024-04-27 21:08:08 +0530

    Update and rename 231)Kth Ancestor in a Tree.java to 231)Kth Ancestor in a Tree-Lowest Common Ancestor.java

--- Tree/231)Kth Ancestor in a Tree-Lowest Common Ancestor.java
+++ Tree/231)Kth Ancestor in a Tree-Lowest Common Ancestor.java
@@ -1,6 +1,6 @@
 Given a binary tree of size  N, a node, and a positive integer k., Your task is to complete the function kthAncestor(), the function should return the kth ancestor of the given node in the binary tree. If there does not exist any such ancestor then return -1.
 Note: It is guaranteed that the node exists in the tree.
-
+Lowest Common Ancestor
 Example 1:
 
 
